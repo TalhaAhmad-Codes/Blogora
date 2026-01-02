b19938bc6e3c9e18c66bea5ddf8cdac0cabb5a60
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-01-02 14:28:12 +0500

    Add DTOs
    Added all DTOs (Base and Filter) for entities.

---
+++ backend/Blogoria/DTOs/UserCommentDTOs/UserCommentFilterDto.cs
@@ -0,0 +1,9 @@
+﻿using Blogoria.DTOs.Common;
+
+namespace Blogoria.DTOs.UserCommentDTOs
+{
+    public class UserCommentFilterDto : BaseFilterDto
+    {
+        public int? UserId { get; init; }
+    }
+}
---
+++ backend/Blogoria/DTOs/UserReactionDTOs/UserReactionFilterDto.cs
@@ -0,0 +1,11 @@
+﻿using Blogoria.DTOs.Common;
+using Blogoria.Models.Enums;
+
+namespace Blogoria.DTOs.UserReactionDTOs
+{
+    public sealed class UserReactionFilterDto : BaseFilterDto
+    {
+        public int? UserId { get; init; }
+        public ReactionType? ReactionType { get; init; }
+    }
+}
